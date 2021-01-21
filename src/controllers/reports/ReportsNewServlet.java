bf6a58b90d6c13d5b2efbbd5111b0639e380f7b8
takenobu.toita <08100911nobu@gmail.com>
2021-01-21 23:42:05 +0900

    Add Report new

--- src/controllers/reports/ReportsNewServlet.java
+++ src/controllers/reports/ReportsNewServlet.java
@@ -1,24 +1,28 @@
-package controllers.login;
+package controllers.reports;
 
 import java.io.IOException;
+import java.sql.Date;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Report;
+
 /**
- * Servlet implementation class LogoutServlet
+ * Servlet implementation class ReportsNewServlet
  */
-@WebServlet("/logout")
-public class LogoutServlet extends HttpServlet {
+@WebServlet("/reports/new")
+public class ReportsNewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public LogoutServlet() {
+    public ReportsNewServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,9 +31,14 @@ public class LogoutServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getSession().removeAttribute("login_employee");
+        request.setAttribute("_token", request.getSession().getId());
 
-        request.getSession().setAttribute("flush", "ログアウトしました。");
-        response.sendRedirect(request.getContextPath() + "/login");
+        Report r = new Report();
+        r.setReport_date(new Date(System.currentTimeMillis()));
+        request.setAttribute("report", r);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/new.jsp");
+        rd.forward(request, response);
     }
+
 }
--- src/models/Report.java
+++ src/models/Report.java
@@ -15,20 +15,16 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-
-/**
- * Servlet implementation class Report
- */
 @Table(name = "reports")
 @NamedQueries({
     @NamedQuery(
-            name = "getAllReports",
-            query = "SELECT r FROM Report AS r ORDER BY r.id DESC"
-            ),
+        name = "getAllReports",
+        query = "SELECT r FROM Report AS r ORDER BY r.id DESC"
+    ),
     @NamedQuery(
-            name = "getAllContent",
-            query = "SELECT COUNT(r) FROM Report AS r"
-            ),
+        name = "getReportsCount",
+        query = "SELECT COUNT(r) FROM Report AS r"
+    ),
 })
 @Entity
 public class Report {
@@ -48,10 +44,10 @@ public class Report {
     private String title;
 
     @Lob
-    @Column(name ="content", nullable = false)
+    @Column(name = "content", nullable = false)
     private String content;
 
-    @Column(name = "create_at", nullable = false)
+    @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
     @Column(name = "updated_at", nullable = false)
@@ -65,55 +61,51 @@ public class Report {
         this.id = id;
     }
 
-
-        public Employee getEmployee(){
-            return employee;
-        }
-
-        public void setEmployee(Employee employee){
-            this.employee = employee;
-        }
-
-        public Date getReport_date() {
-            return report_date;
-        }
-
-        public void setReport_date(Date report_date){
-            this.report_date = report_date;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-
-        }
-
-        public String getContent(){
-            return content;
-        }
-
-        public void setContent(String content){
-            this.content = content;
-        }
-
-        public Timestamp getCreated_at() {
-            return created_at;
-        }
-
-        public void setCreated_at(Timestamp created_at) {
-            this.created_at = created_at;
-
-        }
-
-        public Timestamp getUpdate_at() {
-            return updated_at;
-        }
-
-        public void setUpdated_at(Timestamp updated_at){
-            this.updated_at = updated_at;
-        }
-
+    public Employee getEmployee() {
+        return employee;
     }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Date getReport_date() {
+        return report_date;
+    }
+
+    public void setReport_date(Date report_date) {
+        this.report_date = report_date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+}
